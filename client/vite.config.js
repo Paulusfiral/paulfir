83359e0cfa7bd014e1acfefb8aae4a2b611c3df5
Paulusfiral <firalohoiwutunnn@gmail.com>
2026-07-20 15:39:41 +0700

    Fix: Correct base path from /firelport/ to /paulfir/

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/firelport/",
+  base: "/paulfir/",
   plugins: [react()],
   server: {
     port: 5173,
